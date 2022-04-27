d1d63cad4a97db44fe4fa5d3c77f68a470cd1c00
Rustam Mustafin <rustammuus@mail.ru>
2022-04-27 20:21:45 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2500;
+        int gift = 20;
+        int bonus = price / gift;
+        System.out.println("Начисленные мили: " + bonus);
+    }
+}
